98ab017724603aa8cf4340b9174ebf2d2fc9b949
Riley Flynn <riley@rileyflynn.me>
2020-08-02 01:16:29 -0230

    Set up cors base

--- go.mod
+++ go.mod
@@ -12,4 +12,5 @@ require (
 	github.com/opencontainers/go-digest v1.0.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	golang.org/x/net v0.0.0-20200707034311-ab3426394381 // indirect
+	github.com/gin-contrib/cors v1.3.1
 )
--- service/router.go
+++ service/router.go
@@ -1,10 +1,17 @@
 package service
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
 
 // GetRouter constructs a Gin Router, binds all routes, and then returns the resulting router for later use.
 func GetRouter() *gin.Engine {
 	router := gin.Default()
+	router.Use(cors.New(cors.Config{
+		AllowAllOrigins: true, // TODO: Lock this down.
+		AllowMethods:    []string{"POST"},
+	}))
 
 	router.POST("/process", _ProcessPost)
 
