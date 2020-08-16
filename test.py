818f22cd964f2cf8e6814026c46a60c96d61144b
Riley Flynn <riley@rileyflynn.me>
2020-08-16 14:56:23 -0230

    Remove testing files

--- test.py
+++
@@ -1,9 +0,0 @@
-import json
-
-import requests
-
-
-with open("test.tf") as f:
-    resp = requests.post("http://localhost:9000/process", json={"code": f.read()})
-
-    print(json.dumps(resp.json(), indent=4))
--- test.tf
+++
@@ -1,7 +0,0 @@
-resource "gatsby_text_bold" "test" {
-    text = "Hello world!"
-}
-
-output "contents" {
-    value = gatsby_text_bold.test.contents
-}
