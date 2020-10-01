d6edf23193816dca2398bb69a419a5deff96565a
Riley Flynn <riley@rileyflynn.me>
2020-10-01 19:00:57 -0230

    Add rate limiting

--- service/router.go
+++ service/router.go
@@ -1,13 +1,19 @@
 package service
 
 import (
+	"time"
+
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/ulule/limiter/v3"
+	limitgin "github.com/ulule/limiter/v3/drivers/middleware/gin"
+	"github.com/ulule/limiter/v3/drivers/store/memory"
 )
 
 // GetRouter constructs a Gin Router, binds all routes, and then returns the resulting router for later use.
 func GetRouter() *gin.Engine {
 	router := gin.Default()
+	router.ForwardedByClientIP = true
 	if gin.Mode() == gin.DebugMode {
 		router.Use(cors.New(cors.Config{
 			AllowOrigins: []string{"http://localhost:8000"},
@@ -15,6 +21,15 @@ func GetRouter() *gin.Engine {
 			AllowHeaders: []string{"content-type"},
 		}))
 	}
+
+	rate := limiter.Rate{
+		Period: 1 * time.Minute,
+		Limit:  5,
+	}
+	store := memory.NewStore()
+	limit := limitgin.NewMiddleware(limiter.New(store, rate))
+
+	router.Use(limit)
 
 	router.POST("/process", _ProcessPost)
 
